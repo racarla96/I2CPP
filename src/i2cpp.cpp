4714752e7ef0a163395d163e6b0d4e40a8b7197f
racarla96 <racarla96@gmail.com>
2021-06-30 19:49:34 +0200

    Re implementation!

--- src/i2cpp.cpp
+++ src/i2cpp.cpp
@@ -6,60 +6,73 @@ i2cpp::i2cpp() {
 bool i2cpp::begin(int bus, uint8_t address) {
   this->device = "/dev/i2c-" + std::to_string(bus);
   this->address = address;
-  fd = open(device.c_str(), O_RDWR);
-  if (ioctl(fd, I2C_SLAVE, address) < 0)
+  if (fd = open(device, O_RDWR)) < 0)
     return false;
   else
     return true;
 }
 
 bool i2cpp::writeReg(uint8_t reg, uint8_t *data, int length) {
-  uint8_t t_data[length + 1];
-  
-  t_data[0] = reg;
-  for (int i = 0; i < length; i++)
-    t_data[i + 1] = data[i];
+
+  uint8_t value;
+  if (ioctl(fd, I2C_SLAVE, address) < 0)
+  {
+#if defined(DEBUG)
+    cout << "Failed to acquire bus access and/or talk to slave." << endl;
+#endif
+    return false;;
+  }
+
+  uint8_t buf[2];
+  int ret;
+  for(int i = 0; i < length; i++){
+    value = data[i];
+    buf[0] = reg;
+    buf[1] = value;
+    ret = write(fd, buf, 2);
+    reg++;
+
+    if(ret != 2) return false;
+  }
 
 #if defined(DEBUG)
-  cout << "Write send: ";
+  cout << "Write: ";
   cout << length << " -> ";
-  for(int i = 0; i < length + 1; i++) cout << std::hex << "0x" << unsigned(t_data[i])  << " ";
+  for(int i = 0; i < length + 1; i++) cout << std::hex << "0x" << unsigned(data[i])  << " ";
   cout << endl;
 #endif
 
-  return write(fd, t_data, length + 1) == length + 1;
+  return true;
 }
 
 bool i2cpp::readReg(uint8_t reg, uint8_t *data, int length) {
-  uint8_t t_data[length + 1];
-  for(int i = 0; i < length + 1; i++) t_data[i] = 0;
-  t_data[0] = reg;
-  
+
+  if (ioctl(fd, I2C_SLAVE, address) < 0)
+  {
 #if defined(DEBUG)
-  cout << "Read send: ";
-  cout << length << " -> ";
-  for(int i = 0; i < length + 1; i++) cout << std::hex << "0x" << unsigned(t_data[i])  << " ";
-  cout << endl;
+    cout << "Failed to acquire bus access and/or talk to slave." << endl;
 #endif
-
-  if(read(fd, t_data, length + 1) == (length + 1)){
-    for (int i = 0; i < length; i++)
-      data[i] = t_data[i];
-
-#if defined(DEBUG)
-  cout << "Read recv: ";
-  cout << length << " -> ";
-  for(int i = 0; i < length; i++) cout << std::hex << "0x" << unsigned(t_data[i])  << " ";
-  cout << endl;
-#endif
-
-    return true;
+    return false;;
   }
 
-  return false; 
+  uint8_t u8Ret;
+  for(int i = 0; i<len; i++){
+    write(fd, &reg, 1);
+    read(fd, &u8Ret, 1);
+    buff[i] = u8Ret;
+    reg++;
+  }
+
+#if defined(DEBUG)
+  cout << "Read: ";
+  cout << length << " -> ";
+  for(int i = 0; i < length; i++) cout << std::hex << "0x" << unsigned(data[i])  << " ";
+  cout << endl;
+#endif
+
+  return true; 
 }
 
 bool i2cpp::close(void) {
-  //return close(fd);
-  return true;
+  return close(fd);
 }
